07e214be3c6a7422c47e0c687a440cc2bcdc5b53
ruanazevedo <ruannzeroh@gmail.com>
2026-06-03 22:18:41 -0300

    http server para GetById de Artista implementado

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"azevedoruan.github/lbd-gorm/cmd/config"
 	"azevedoruan.github/lbd-gorm/internal/controller"
 	"azevedoruan.github/lbd-gorm/internal/repository"
@@ -9,12 +12,19 @@ import (
 func main() {
 	db := config.DBSetup()
 
-	userRepo := repository.UserRepository{DB: db}
 	artistRepo := repository.ArtistRepository{DB: db}
-	musicRepo := repository.MusicRepository{DB: db}
-	playlistRepo := repository.PlaylistRepository{DB: db}
-	controller.GetUserByID(&userRepo)
-	controller.GetArtistByID(&artistRepo)
-	controller.GetMusicByID(&musicRepo)
-	controller.GetPlaylistByID(&playlistRepo)
+	artistContr := controller.ArtistController{Respo: &artistRepo}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{id}", artistContr.GetByID)
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
+
+	//userRepo := repository.UserRepository{DB: db}
+	//musicRepo := repository.MusicRepository{DB: db}
+	//playlistRepo := repository.PlaylistRepository{DB: db}
+	//controller.GetUserByID(&userRepo)
+	//controller.GetArtistByID(&artistRepo)
+	//controller.GetMusicByID(&musicRepo)
+	//controller.GetPlaylistByID(&playlistRepo)
 }
--- internal/controller/ArtistController.go
+++ internal/controller/ArtistController.go
@@ -1,17 +1,40 @@
 package controller
 
 import (
-	"fmt"
-	"log"
+	"encoding/json"
+	"net/http"
+	"strconv"
 
 	"azevedoruan.github/lbd-gorm/internal/repository"
 )
 
-// Uma função hipotética para requisição HTTP
-func GetArtistByID(repo *repository.ArtistRepository) {
-	artist, err := repo.FindByID(2)
-	if err != nil {
-		log.Fatalf("Não foi possível buscar o artista pelo ID %d\n", 2)
+type ArtistController struct {
+	Respo *repository.ArtistRepository
+}
+
+// Constructor
+func (ArtistController) NewArtistController(respo *repository.ArtistRepository) *ArtistController {
+	return &ArtistController{Respo: respo}
+}
+
+func (e *ArtistController) GetByID(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Método não permitido", http.StatusBadRequest)
+		return
 	}
-	fmt.Printf("Artista buscado: %d, %s, %s\n", artist.ID, artist.Name, artist.Nacionality)
+
+	id, err := strconv.ParseUint(r.PathValue("id"), 10, 64)
+	if err != nil {
+		http.Error(w, "Error to get ID", http.StatusBadRequest)
+		return
+	}
+
+	obj, err := e.Respo.FindByID(uint(id))
+	if err != nil {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(obj)
 }
