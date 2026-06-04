15272678bfc7e70cbef602bcb95c00385c4ea90f
ruanazevedo <ruannzeroh@gmail.com>
2026-06-04 16:01:20 -0300

    GetById de User implementado

--- internal/controller/UserController.go
+++ internal/controller/UserController.go
@@ -1,17 +1,39 @@
 package controller
 
 import (
-	"fmt"
-	"log"
+	"encoding/json"
+	"net/http"
+	"strconv"
 
 	"azevedoruan.github/lbd-gorm/internal/repository"
 )
 
-// Uma função hipotética para requisição HTTP
-func GetUserByID(repo *repository.UserRepository) {
-	user, err := repo.FindByID(2)
-	if err != nil {
-		log.Fatalf("Não foi possível buscar o usuário pelo ID %d\n", 2)
+type UserController struct {
+	Respo *repository.UserRepository
+}
+
+func (e *UserController) Setup(mux *http.ServeMux) {
+	mux.HandleFunc("/usuario/{id}", e.GetById)
+}
+
+func (e *UserController) GetById(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed. Require GET", http.StatusBadRequest)
+		return
 	}
-	fmt.Printf("Usuario buscado: %d, %s, %s\n", user.ID, user.Name, user.Email)
+
+	id, err := strconv.ParseUint(r.PathValue("id"), 10, 64)
+	if err != nil {
+		http.Error(w, "Error to get ID", http.StatusBadRequest)
+		return
+	}
+
+	obj, err := e.Respo.FindByID(uint(id))
+	if err != nil {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(obj)
 }
--- internal/repository/UserRepo.go
+++ internal/repository/UserRepo.go
@@ -15,3 +15,24 @@ func (r *UserRepository) FindByID(id uint) (models.User, error) {
 	err := r.DB.First(&user, id).Error
 	return user, err
 }
+
+func (r *UserRepository) FindAll() ([]models.User, error) {
+	var users []models.User
+	err := r.DB.Find(&users).Error
+	return users, err
+}
+
+func (r *UserRepository) AddNew(newUser models.User) (models.User, error) {
+	result := r.DB.Create(&newUser)
+	return newUser, result.Error
+}
+
+func (r *UserRepository) Update(updatedUser models.User) error {
+	err := r.DB.Save(&updatedUser).Error
+	return err
+}
+
+func (r *UserRepository) DeleteById(id uint) error {
+	err := r.DB.Delete(&models.User{}, id).Error
+	return err
+}
